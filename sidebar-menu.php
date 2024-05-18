69c7ef2f9a90794088cda0a1cd826611f44bd778
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-05-18 16:19:56 +0700

    update hak akses

--- sidebar-menu.php
+++ sidebar-menu.php
@@ -18,16 +18,18 @@
   <li class="<?= $_GET["module"] == "beranda" ? 'active' : '' ?>">
     <a href="?module=beranda"><i class="fa fa-home"></i> Beranda </a>
   </li>
-  <li class="<?= $_GET["module"] == "obat" || $_GET["module"] == "form_obat" ? 'active' : '' ?>">
-    <a href="?module=obat"><i class="fa fa-folder"></i> Data Obat </a>
-  </li>
-  <li class="<?= $_GET["module"] == "obat_masuk" || $_GET["module"] == "form_obat_masuk" ? 'active' : '' ?>">
-    <a href="?module=obat_masuk"><i class="fa fa-clone"></i> Data Obat Masuk </a>
-  </li>
-  <li class="<?= $_GET["module"] == "obat_keluar" || $_GET["module"] == "form_obat_keluar" ?>">
-    <a href="?module=obat_keluar"><i class="fa fa-clone"></i> Data Obat Keluar </a>
-  </li>
-  <?php if ($_SESSION['hak_akses'] == 'Super Admin') : ?>
+  <?php if ($_SESSION['hak_akses'] == 'Owner' || $_SESSION['hak_akses'] == 'Gudang' || $_SESSION['hak_akses'] == 'Admin') : ?>
+    <li class="<?= $_GET["module"] == "obat" || $_GET["module"] == "form_obat" ? 'active' : '' ?>">
+      <a href="?module=obat"><i class="fa fa-th"></i> Data Obat </a>
+    </li>
+    <li class="<?= $_GET["module"] == "obat_masuk" || $_GET["module"] == "form_obat_masuk" ? 'active' : '' ?>">
+      <a href="?module=obat_masuk"><i class="fa fa-sign-in"></i> Data Obat Masuk </a>
+    </li>
+    <li class="<?= $_GET["module"] == "obat_keluar" || $_GET["module"] == "form_obat_keluar" ? 'active' : '' ?>">
+      <a href="?module=obat_keluar"><i class="fa fa-sign-out"></i> Data Obat Keluar </a>
+    </li>
+  <?php endif ?>
+  <?php if ($_SESSION['hak_akses'] == 'Owner' || $_SESSION['hak_akses'] == 'Admin') : ?>
     <li class="<?= $_GET["module"] == "lap_stok" || $_GET["module"] == "lap_obat_masuk" || $_GET["module"] == "lap_obat_keluar" ? 'active' : '' ?> treeview">
       <a href="javascript:void(0);">
         <i class="fa fa-file-text"></i> <span>Laporan</span> <i class="fa fa-angle-left pull-right"></i>
@@ -38,10 +40,13 @@
         <li class="<?= $_GET["module"] == "lap_obat_keluar" ? 'active' : '' ?>"><a href="?module=lap_obat_keluar"><i class="fa fa-circle-o"></i> Obat Keluar </a></li>
       </ul>
     </li>
+  <?php endif ?>
+  <?php if ($_SESSION['hak_akses'] == 'Admin') : ?>
     <li class="<?= $_GET["module"] == "user" || $_GET["module"] == "form_user" ? 'active' : '' ?>">
       <a href="?module=user"><i class="fa fa-user"></i> Manajemen User</a>
     </li>
   <?php endif ?>
+
   <li class="<?= $_GET["module"] == "password" ? 'active' : '' ?>">
     <a href="?module=password"><i class="fa fa-lock"></i> Ubah Password</a>
   </li>
